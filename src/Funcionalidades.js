40e7681dab6fa1f448e1fbd9ad4b1efaa2bc98e9
Alfred Garcia <alfred.garcia@ucb.edu.bo>
2022-03-07 17:23:53 -0400

    Seleccionar estado y mostrar el porcentaje de impuesto que tiene ese estado

--- src/Funcionalidades.js
+++ src/Funcionalidades.js
@@ -4,11 +4,42 @@ const estado=document.querySelector("#estado-input");
 const form = document.querySelector("#totalizador-form");
 const div = document.querySelector("#resultado-div");
 
-form.addEventListener("submit", (event) => {
+form.addEventListener("submit", (event) => 
+{
   event.preventDefault();
 
+  let valorimpuesto = ImpFORstate(estado.value);
   const cantidadMostrar = Number.parseInt(cantidad.value);
   const precioMostrar = Number.parseInt(precio.value);
 
-  div.innerHTML = "<p>" + cantidadMostrar + "<p>" + "<p>" + precioMostrar + "</p>";
+
+  div.innerHTML = "<p>" + cantidadMostrar + "<p>" + "<p>" + precioMostrar + "<p>"
+  document.getElementById('impuesto').innerHTML = valorimpuesto;
 }); 
+
+function ImpFORstate(valueState) 
+{
+  let valorImpuestos=0;
+  //let res = precioTotal(cantidad.value, precio.value);
+  switch (valueState) 
+  {
+      case 'UT':
+          valorImpuestos = 6.65;
+          break;
+      case 'NV':
+          valorImpuestos = 8.00;
+          break;
+      case 'TX':
+          valorImpuestos = 6.25;
+          break;
+      case 'AL':
+          valorImpuestos = 4.00;
+          break;
+      case 'CA':
+          valorImpuestos = 8.25;
+          break;
+      default:
+          alert("No existe");
+  }
+  return valorImpuestos;
+}
