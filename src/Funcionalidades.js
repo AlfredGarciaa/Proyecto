031c8aa099b88482bb4113d2a2f661ed93ec7698
Alfred Garcia <alfred.garcia@ucb.edu.bo>
2022-03-07 18:21:45 -0400

    Funcionalidades completas

--- src/Funcionalidades.js
+++ src/Funcionalidades.js
@@ -2,78 +2,76 @@ const cantidad = document.querySelector("#cantidad-input");
 const precio = document.querySelector("#precio-input");
 const estado=document.querySelector("#estado-input");
 const form = document.querySelector("#totalizador-form");
-const div = document.querySelector("#resultado-div");
-
-form.addEventListener("submit", (event) => 
-{
-  event.preventDefault();
-
-  let res= precioTotal(cantidad.value,precio.value);
-  let valorimpuesto = ImpFORstate(estado.value);
-  let valordescuento=Descuento(res+valorimpuesto);
-  const cantidadMostrar = Number.parseInt(cantidad.value);
-  const precioMostrar = Number.parseInt(precio.value);
 
 
-  div.innerHTML = "<p>" + cantidadMostrar + "<p>" + "<p>" + precioMostrar + "<p>"
-  document.getElementById('descuento').innerHTML=valordescuento
-  document.getElementById('impuesto').innerHTML = valorimpuesto;
-  document.getElementById('precio').innerHTML=res;
-  document.getElementById('total').innerHTML = res+valorimpuesto;
-}); 
+form.addEventListener("submit", (event)=>{
+    event.preventDefault();
+    let res= precioTotal(cantidad.value,precio.value);
+    let valorimpuesto = ImpFORstate(res,estado.value);
+    let valordescuento=Descuento(res+valorimpuesto);
 
-function ImpFORstate(valueState) 
-{
-  let valorImpuestos=0;
-  //let res = precioTotal(cantidad.value, precio.value);
-  switch (valueState) 
-  {
-    case 'CA':
-        valorImpuestos = 4.00;
-        break;
-    case 'TX':
-        valorImpuestos = 0.0625;
-        break;
-    case 'AL':
-        valorImpuestos = 0.04;
-        break;
-    case 'NV':
-        valorImpuestos = 0.08;
-        break;
-    case 'UT':
-        valorImpuestos = 0.0665;
-        break;
-    default:
-        alert("No existe");
-  }
-  return valorImpuestos;
-}
+    document.getElementById('precio').innerHTML=res
+    //alert("Codigo de estado: "+estado.value+"\n"+"Valor Impuesto: "+valorimpuesto);
+    //alert("Cantidad: "+cantidad.value+"\n"+"Precio: "+precio.value+"\n"+"PrecioTotal: "+res);
+    document.getElementById('descuento').innerHTML=valordescuento
+    document.getElementById('impuesto').innerHTML = valorimpuesto;
+    document.getElementById('total').innerHTML = res+valorimpuesto-valordescuento;
+});
 
-function precioTotal(cantidad,precio)
-{
+function precioTotal(cantidad,precio){
     let res=cantidad*precio;
     return res;
 }
 
-function Descuento(valorcompra)
-{
+function ImpFORstate(valorcompra, valueState) {
+    let valorImpuestos=0;
+    let res;
+    //let res = precioTotal(cantidad.value, precio.value);
+    switch (valueState) {
+        case 'UT':
+            valorImpuestos = 0.0665;
+            break;
+        case 'NV':
+            valorImpuestos = 0.08;
+            break;
+        case 'TX':
+            valorImpuestos = 0.0625;
+            break;
+        case 'AL':
+            valorImpuestos = 0.04;
+            break;
+        case 'CA':
+            valorImpuestos = 0.0825;
+            break;
+        default:
+            alert("No existe");
+    }
+    res = valorcompra * valorImpuestos;
+    return res;
+}
+
+function Descuento(valorcompra){
     let valorDescuento=0;
     let res;
-    if(valorcompra>7000)
-    {
-        valorDescuento=0.07;
-    }else
-    {
-        if(valorcompra>3000)
-        {
-            valorDescuento=0.05;
-        }else
-        {
-            if(valorcompra>1000)
-            {
-                valorDescuento=0.03
+    if(valorcompra>30000){
+        valorDescuento=0.15;
+    }else{
+        if(valorcompra>10000){
+            valorDescuento=0.1;
+        }else{
+            if(valorcompra>7000){
+                valorDescuento=0.07;
+            }else{
+                if(valorcompra>3000){
+                    valorDescuento=0.05;
+                }else{
+                    if(valorcompra>1000)
+                    {
+                        valorDescuento=0.03
+                    }
+                }
             }
         }
     }
-    return valorDescuento;
+    return res = valorcompra*valorDescuento;
 }
