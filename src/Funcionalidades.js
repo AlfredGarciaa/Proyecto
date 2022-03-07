a6544f4cc9e1d75128762ae7b603fc65b6c44459
Alfred Garcia <alfred.garcia@ucb.edu.bo>
2022-03-07 16:41:10 -0400

    Seleccionar estado y mostrar elección

--- src/Funcionalidades.js
+++ src/Funcionalidades.js
@@ -1,5 +1,6 @@
 const cantidad = document.querySelector("#cantidad-input");
 const precio = document.querySelector("#precio-input");
+const estado=document.querySelector("#estado-input");
 const form = document.querySelector("#totalizador-form");
 const div = document.querySelector("#resultado-div");
 
@@ -9,5 +10,5 @@ form.addEventListener("submit", (event) => {
   const cantidadMostrar = Number.parseInt(cantidad.value);
   const precioMostrar = Number.parseInt(precio.value);
 
-  div.innerHTML = "<p>" + cantidadMostrar + "<p>" + "\n" + "<p>" + precioMostrar + "</p>";
+  div.innerHTML = "<p>" + cantidadMostrar + "<p>" + "<p>" + precioMostrar + "</p>";
 }); 
